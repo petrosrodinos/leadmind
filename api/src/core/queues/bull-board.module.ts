cf2bab7af20a49ab7dec27fd0642114eb184cf2c
Petros Rodinos <petros.rodinos@yahoo.com>
2026-06-30 20:30:47 +0300

    bullmq

--- api/src/core/queues/bull-board.module.ts
+++ api/src/core/queues/bull-board.module.ts
@@ -4,8 +4,15 @@ import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
 import { ExpressAdapter } from '@bull-board/express';
 import { Queue } from 'bullmq';
 import { BullModule, getQueueToken } from '@nestjs/bullmq';
-import { AI_PROCESS_QUEUE, BULL_BOARD_ADAPTER, FILTER_SCRAPE_QUEUE } from './queues.constants';
-
+import {
+    AI_PROCESS_QUEUE,
+    BULL_BOARD_ADAPTER,
+    FILTER_SCRAPE_QUEUE,
+    MARKETING_CAMPAIGN_DISPATCH_QUEUE,
+    MARKETING_MESSAGE_SEND_QUEUE,
+    OUTREACH_SEND_QUEUE,
+    REMINDER_TRIGGER_QUEUE,
+} from './queues.constants';
 
 @Global()
 @Module({
@@ -13,6 +20,10 @@ import { AI_PROCESS_QUEUE, BULL_BOARD_ADAPTER, FILTER_SCRAPE_QUEUE } from './que
         BullModule.registerQueue(
             { name: FILTER_SCRAPE_QUEUE },
             { name: AI_PROCESS_QUEUE },
+            { name: OUTREACH_SEND_QUEUE },
+            { name: MARKETING_CAMPAIGN_DISPATCH_QUEUE },
+            { name: MARKETING_MESSAGE_SEND_QUEUE },
+            { name: REMINDER_TRIGGER_QUEUE },
         ),
     ],
     providers: [
@@ -21,8 +32,19 @@ import { AI_PROCESS_QUEUE, BULL_BOARD_ADAPTER, FILTER_SCRAPE_QUEUE } from './que
             inject: [
                 getQueueToken(FILTER_SCRAPE_QUEUE),
                 getQueueToken(AI_PROCESS_QUEUE),
+                getQueueToken(OUTREACH_SEND_QUEUE),
+                getQueueToken(MARKETING_CAMPAIGN_DISPATCH_QUEUE),
+                getQueueToken(MARKETING_MESSAGE_SEND_QUEUE),
+                getQueueToken(REMINDER_TRIGGER_QUEUE),
             ],
-            useFactory: (filterScrapeQueue: Queue, aiProcessQueue: Queue) => {
+            useFactory: (
+                filterScrapeQueue: Queue,
+                aiProcessQueue: Queue,
+                outreachSendQueue: Queue,
+                marketingCampaignDispatchQueue: Queue,
+                marketingMessageSendQueue: Queue,
+                reminderTriggerQueue: Queue,
+            ) => {
                 const serverAdapter = new ExpressAdapter();
                 serverAdapter.setBasePath('/admin/queues');
 
@@ -30,6 +52,10 @@ import { AI_PROCESS_QUEUE, BULL_BOARD_ADAPTER, FILTER_SCRAPE_QUEUE } from './que
                     queues: [
                         new BullMQAdapter(filterScrapeQueue),
                         new BullMQAdapter(aiProcessQueue),
+                        new BullMQAdapter(outreachSendQueue),
+                        new BullMQAdapter(marketingCampaignDispatchQueue),
+                        new BullMQAdapter(marketingMessageSendQueue),
+                        new BullMQAdapter(reminderTriggerQueue),
                     ],
                     serverAdapter,
                 });
